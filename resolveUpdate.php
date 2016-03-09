4e61aa3017a67f52adbd1c0e299324a8cd434c71
adsamcik <adsamcik@gmail.com>
2016-03-09 21:13:42 +0100

    Realtime updates WIP

--- resolveUpdate.php
+++ resolveUpdate.php
@@ -1,14 +1,31 @@
 <?php
+
+function bigintval($value) {
+  $value = trim($value);
+  if (ctype_digit($value)) {
+    return $value;
+  }
+  $value = preg_replace("/[^0-9](.*)$/", '', $value);
+  if (ctype_digit($value)) {
+    return $value;
+  }
+  return 0;
+}
+
 	$fileName = "data.txt";
+	$timeMilisec = floor(microtime(true)*1000);
+	$timeSec = $timeMilisec * 1000;
+
 	$handle = fopen($fileName, "c+");
 	$fileSize = filesize($fileName);
 	if($fileSize > 0) {
 		$text = fread($handle, $fileSize);
 		ftruncate($handle, 0);
+		rewind($handle);
 	}
 	$array = json_decode($_POST["data"], true);
 	foreach($array as $item) {
-		var_dump($item);
+		//var_dump($item);
 		if(isset($item["end"]))
 			$count = $item["end"] - $item["start"];
 		else
@@ -20,12 +37,50 @@
 				$text = substr_replace($text, "", $item["start"], $count);
 		}
 		else {
-			echo "value:" . $item["value"] . "start" . $item["start"];
+			//echo "value:" . $item["value"] . "start" . $item["start"];
 			$text = substr_replace($text, $item["value"], $item["start"], $count);
 		}
 	}
-	rewind($handle);
+
 	fwrite($handle, $text);
 	fclose($handle);
-	echo "\nBEGIN\n";
-	echo $text;
+	
+	if(file_exists($fileName.'.cache')) $cache = json_decode(file_get_contents($fileName.'.cache'), true);
+
+	if($cache === null) $cache = array();
+
+	$changes = array();
+	$length = count($cache);
+	$lastCheck = bigintval($_POST['last']);
+	$minuteAgo = $timeMilisec - 60000;
+
+	if($lastCheck < $minuteAgo) {
+		$result['last'] = $lastCheck;
+		$result['data'] = $text;
+		$result['time'] = $timeMilisec;
+		exit(json_encode($result));
+	}
+
+	if(is_array($cache)) {
+	foreach ($cache as $val) {
+		if($timeMilisec - $val['time'] > 60000)
+			unset($val);
+		else if($val['time'] > $lastCheck)
+			array_push($changes, $val);
+	}
+	}
+
+	$length = count($array);
+	for($i = 0; $i < $length; $i++) {
+		$object = $array[$i];
+		$object['time'] = $timeMilisec;
+		array_push($cache, $object);
+	}
+
+	file_put_contents($fileName.'.cache', json_encode(array_values($cache)));
+
+	$result['data'] = $changes;
+	$result['time'] = $timeMilisec;
+	exit(json_encode($result));
+		//echo "\nBEGIN\n";
+	//echo $text;
--- script.js
+++ script.js
@@ -3,11 +3,12 @@ var ta = document.getElementById("textarea");
 
 //ta.value = "Hey I am really here";
 ta.focus();
-ta.selectionStart = 5;
+//ta.selectionStart = 5;
 ta.addEventListener("keydown", update);
 
 var stack = [];
 var previousStart;
+var last;
 
 promise.get("data.txt", null, null).then(function (error, text, xhr) {
 	ta.textContent = text;
@@ -17,11 +18,14 @@ function update(e) {
 	var event = window.event ? window.event : e;
 	console.log(event.keyCode);
 	if (event.key == "Backspace") resolveBackspace(event.key);
-	else if (isValidKey(event.keyCode)) resolveNewKey(event.key);
+	else if (isValidKey(event)) resolveNewKey(event.key);
 }
 
-function isValidKey(key) {
-	return key != 16 && key != 17 && key != 18 && key != 20 && key != 37 && key != 38 && key != 39 && key != 40 && !(key > 112 && key < 123);
+function isValidKey(e) {
+	var key = e.keyCode;
+	if (e.ctrlKey || e.altKey)
+		return false;
+	return key != 16 && key != 17 && key != 18 && key != 20 && key != 37 && key != 38 && key != 39 && key != 40 && !(key >= 112 && key <= 123);
 }
 
 function resolveNewKey(key) {
@@ -50,22 +54,44 @@ function send() {
 			Accept: "application/json"
 		};
 		promise.post("resolveUpdate.php", {
-			data: JSON.stringify(stack)
+			data: JSON.stringify(stack),
+			last: last
 		}, xhr).then(
 			function (error, text, xhr) {
 				if (error) {
 					alert('Error ' + xhr.status);
 					return;
 				}
-				console.log(text);
+				var json = JSON.parse(text);
+				if (json.time != null)
+					last = json.time;
+
+				if (typeof json.data === 'string' || json.data.length === 0)
+					return;
+
+				var text = ta.textContent;
+				for (var i = 0; i < json.data.length; i++) {
+					var data = json.data[i];
+					console.log(data.value);
+					var count = data.end - data.start;
+					if (data.value == "Backspace") {
+						if (count == 0)
+							text = text.substr(data.start - 1, 1);
+						else
+							text = text.substr(data.start, count);
+					} else {
+						//echo "value:" . $item["value"] . "start" . $item["start"];
+						text = text.slice(0, data.start) + data.value + text.slice(data.end, text.length - data.end);
+					}
+					console.log(text);
+				}
+				ta.value = text;
+				console.log(json);
 			}
 		);
 		stack.length = 0;
 	}
 }
 
-function recieve() {
-
-}
 
 setInterval(send, 1000);
