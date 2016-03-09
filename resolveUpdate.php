a9a83021c7e271a2fff93029a9854a77e60f5e74
adsamcik <adsamcik@gmail.com>
2016-03-09 14:17:52 +0100

    Writting should work
    Todo support ctrl+a etc

--- resolveUpdate.php
+++ resolveUpdate.php
@@ -4,25 +4,28 @@
 	$fileSize = filesize($fileName);
 	if($fileSize > 0) {
 		$text = fread($handle, $fileSize);
-		ftruncate($handle, $filesize);
+		ftruncate($handle, 0);
 	}
 	$array = json_decode($_POST["data"], true);
 	foreach($array as $item) {
 		var_dump($item);
-		if($item["value"] == "Backspace") {
+		if(isset($item["end"]))
 			$count = $item["end"] - $item["start"];
+		else
+			$count = 0;
+		if($item["value"] == "Backspace") {
 			if($count == 0)
 				$text = substr_replace($text, "", $item["start"]-1, 1);
 			else
-				$text = substr_replace($text, "", $item["start"], $item["end"] - $item["start"]);
+				$text = substr_replace($text, "", $item["start"], $count);
 		}
 		else {
 			echo "value:" . $item["value"] . "start" . $item["start"];
-			$text = substr_replace($text, $item["value"], $item["start"], 0);
+			$text = substr_replace($text, $item["value"], $item["start"], $count);
 		}
 	}
+	rewind($handle);
 	fwrite($handle, $text);
 	fclose($handle);
+	echo "\nBEGIN\n";
 	echo $text;
-	
-	
--- script.js
+++ script.js
@@ -9,28 +9,49 @@ ta.addEventListener("keydown", update);
 var stack = [];
 var previousStart;
 
+promise.get("data.txt", null, null).then(function (error, text, xhr) {
+	ta.textContent = text;
+});
+
 function update(e) {
 	var event = window.event ? window.event : e;
+	console.log(event.keyCode);
 	if (event.key == "Backspace") resolveBackspace(event.key);
-	else resolveNewKey(event.key);
+	else if (isValidKey(event.keyCode)) resolveNewKey(event.key);
+}
+
+function isValidKey(key) {
+	return key != 16 && key != 17 && key != 18 && key != 20 && key != 37 && key != 38 && key != 39 && key != 40 && !(key > 112 && key < 123);
 }
 
 function resolveNewKey(key) {
 	if (stack.length > 0 && ta.selectionStart == ++previousStart)
 		stack[stack.length - 1].value += key;
 	else
-		stack.push({ value: key, start: ta.selectionStart });
+		stack.push({
+			value: key,
+			start: ta.selectionStart,
+			end: ta.selectionEnd
+		});
 	previousStart = ta.selectionStart;
 }
 
 function resolveBackspace(key) {
-	stack.push({ value: key, start: ta.selectionStart, end: ta.selectionEnd });
+	stack.push({
+		value: key,
+		start: ta.selectionStart,
+		end: ta.selectionEnd
+	});
 }
 
 function send() {
 	if (stack.length > 0) {
-		var xhr = { Accept: "application/json" };
-		promise.post("resolveUpdate.php", {data: JSON.stringify(stack)}, xhr).then(
+		var xhr = {
+			Accept: "application/json"
+		};
+		promise.post("resolveUpdate.php", {
+			data: JSON.stringify(stack)
+		}, xhr).then(
 			function (error, text, xhr) {
 				if (error) {
 					alert('Error ' + xhr.status);
@@ -38,7 +59,7 @@ function send() {
 				}
 				console.log(text);
 			}
-			);
+		);
 		stack.length = 0;
 	}
 }
