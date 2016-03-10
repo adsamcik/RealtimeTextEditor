5eca153badb8326cc69340566f7b3abff39343f7
adsamcik <adsamcik@gmail.com>
2016-03-10 20:41:00 +0100

    Improved filename detection.. now uses # instead of ?

--- js/script.js
+++ js/script.js
@@ -1,19 +1,16 @@
-function getParameterByName(name, url) {
-	if (!url) url = window.location.href;
-	url = url.toLowerCase(); // This is just to avoid case sensitiveness  
-	name = name.replace(/[\[\]]/g, "\\$&");
-	var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
-		results = regex.exec(url);
-	if (!results) return null;
-	if (!results[2]) return '';
-	return decodeURIComponent(results[2].replace(/\+/g, " "));
+function getFileName() {
+	var url = window.location.href;
+	var indexOf = url.indexOf('#', 0);
+	if (indexOf + 1 == url.length)
+		return null;
+	return url.substr(indexOf + 1, url.length - indexOf);
 }
 
 var ta = document.getElementById("textarea");
 var stack = [];
 var previousStart;
 var last;
-var fileName = getParameterByName('file');
+var fileName = getFileName();
 
 if (fileName === null) {
 	ta.disabled = true;
